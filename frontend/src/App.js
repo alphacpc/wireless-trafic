db5062263ea3df195d8c4524e100d50afa498002
alphacpc <alphacpc@gmail.com>
2022-08-30 16:35:42 +0000

    Remove some files and updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var liste = [1,2,3,4,5,6,7,8,1,2,2,2,2]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="divAppContainer">
+      <div className="divTableStream">
+        <table className="table">
+
+          <thead>
+            <tr>
+              <th>Numero</th>
+              <th>Temps</th>
+              <th>Source</th>
+              <th>Destination</th>
+              <th>Protocol</th>
+              <th>Information</th>
+            </tr>
+          </thead>
+
+          <tbody>
+           {liste.map(el =>  <tr key={el}>
+              <td>444</td>
+              <td>Mardi 30 Aout, 16:44:23</td>
+              <td>192.X.X.X</td>
+              <td>192.X.X.X</td>
+              <td>DNS</td>
+              <td>www.google.com</td>
+            </tr>)}
+          </tbody>
+
+        </table>
+      </div>
     </div>
   );
 }
