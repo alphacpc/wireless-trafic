5810dc57f1aca54e4758fb4c0ab4694ea3087f48
alphacpc <alphacpc@gmail.com>
2022-08-31 12:35:45 +0000

    Updated project and organize

--- test.py
+++ test.py
@@ -1,31 +1,33 @@
 import pyshark
 import time
+import geocoder
 
-# define interface
+# Define Interface
 networkInterface = "wlp0s20f3"
 
-# define capture object
+# Define Capture
 capture = pyshark.LiveCapture(interface=networkInterface)
 
-print("listening on %s" % networkInterface)
-
-for packet in capture:
+for packet in capture.sniff_continuously(packet_count=200):
     try:
         localtime = time.asctime(time.localtime(time.time()))
      
-        protocol = packet.transport_layer   # protocol type
-        src_addr = packet.ip.src            # source address
-        src_port = packet[protocol].srcport   # source port
-        dst_addr = packet.ip.dst            # destination address
-        dst_port = packet[protocol].dstport   # destination port
-        
-        if "DNS" in packet and not packet.dns.flags_response.int_value:
-            print(packet.dns.qry_name)
+        protocol = packet.transport_layer
+        src_addr = packet.ip.src
+        src_port = packet[protocol].srcport
+        dst_addr = packet.ip.dst
+        dst_port = packet[protocol].dstport
 
-        print ("%s --- IP %s:%s --- %s:%s --- (%s)" % (localtime, src_addr, src_port, dst_addr, dst_port, protocol))
+        ip = geocoder.ip(dst_addr)
+        country_name = ip.country
+        city_name = ip.city
+        state_name = ip.state
+        location = ip.latlng
+
+        domaine_name = packet.dns.qry_name if ("DNS" in packet and not packet.dns.flags_response.int_value) else None
+
+        print ("%s --- IP %s:%s --- %s:%s --- (%s) --- (%s) --- (%s) --- %s --- %s" % (localtime, src_addr, src_port, dst_addr, dst_port, protocol, country_name, city_name, state_name, location))
 
     except AttributeError as e:
-        # ignore packets other than TCP, UDP and IPv4
+        # IGNORE PACKETs !=  TCP, UDP and IPv4
         pass
-
-    print (" ")
