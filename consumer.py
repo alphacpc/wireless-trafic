ea7b117d02759eb108193bda1ada6880b57d01cd
alphacpc <alphacpc@gmail.com>
2022-09-05 20:05:16 +0000

    Updated data with added today and isDNS

--- consumer.py
+++ consumer.py
@@ -1,5 +1,8 @@
 from kafka3 import KafkaConsumer
 from elasticsearch6 import Elasticsearch
+from datetime import datetime
+
+from pprint import pprint
 
 consumer = KafkaConsumer("trafic", bootstrap_servers='localhost:9092')
 
@@ -14,17 +17,39 @@ for msg in consumer:
 
     response = msg.value.decode("utf-8").split("-")
 
-    doc = {
-        "localtime": response[0].strip(),
-        "protocol": response[1].strip(),
-        "src_addr": response[2].strip(),
-        "src_port": response[3].strip(),
-        "dst_addr": response[4].strip(),
-        "dst_port": response[5].strip(),
-        "information": response[6].strip(),
-    }
-    
-    elastic.index(index="captures", body=doc, doc_type="_doc")
+    print(response, "\n")
+
+    if len(response) == 9:
+
+        # doc = {
+        #     "localtime": response[0].strip(),
+        #     "today": response[1].strip(),
+        #     "src_addr": response[2].strip(),
+        #     "src_port": response[5].strip(),
+        #     "dst_addr": response[4].strip(),
+        #     "dst_port": response[3].strip(),
+        #     "protocol": response[6].strip(),
+        #     "information": response[7].strip(),
+        #     "isDNS": response[8].strip(),
+        # }
+        
+        elastic.index(
+            index="captures", 
+            body = {
+                "localtime": response[0].strip(),
+                "today": response[1].strip(),
+                "src_addr": response[2].strip(),
+                "src_port": response[5].strip(),
+                "dst_addr": response[4].strip(),
+                "dst_port": response[3].strip(),
+                "protocol": response[6].strip(),
+                "information": response[7].strip(),
+                "isDNS": response[8].strip(),
+            }, 
+            doc_type = "_doc"
+        )
+
+
 
 
 print("Consumer finish !!!")
--- producer.py
+++ producer.py
@@ -1,5 +1,6 @@
 import pyshark
 import time
+from datetime import datetime
 from kafka3 import KafkaProducer
 
 
@@ -17,16 +18,17 @@ print("Programme tourne...")
 for packet in capture:
     try:
         localtime = time.asctime(time.localtime(time.time()))
-     
+        today = datetime.today().strftime('%Y/%m/%d')
         protocol = packet.transport_layer
         src_addr = packet.ip.src
         src_port = packet[protocol].srcport
         dst_addr = packet.ip.dst
         dst_port = packet[protocol].dstport
         domaine_name = packet.dns.qry_name if ("DNS" in packet and not packet.dns.flags_response.int_value) else packet.length
+        domaine_existe = True if ("DNS" in packet and not packet.dns.flags_response.int_value) else False
 
 
-        producer.send('trafic', bytes("%s - %s - %s - %s - %s - %s -%s"%(localtime, src_addr, src_port, dst_addr, dst_port, protocol, domaine_name), 'utf-8'))
+        producer.send('trafic', bytes("%s - %s - %s - %s - %s - %s - %s -%s - %s" % (localtime, today, src_addr, src_port, dst_addr, dst_port, protocol, domaine_name, domaine_existe), 'utf-8'))
  
 
     except AttributeError as e:
